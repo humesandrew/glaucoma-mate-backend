218246620d137f5ca7bce64dda1dae1f3faca446
Andy Humes <andrew.humes@du.edu>
2023-07-06 20:37:15 -0600

    addfirstroutes

--- backend/server.js
+++ backend/server.js
@@ -3,4 +3,16 @@ const app = express();
 
 require('dotenv').config();
 
-app.listen(4000, () => console.log('listening on port 4000.'));
+
+// establish middle ware to monitor requests to server and response//
+app.use((req, res, next) => {
+  console.log("Path is " + req.path + " and method is " + req.method)
+})
+
+//routes /////////////////////////////
+app.get('/', (req, res) => {
+  res.json({mssg:'Route is / and method is ' + req.method})
+}
+);
+
+app.listen(process.env.PORT, () => console.log('listening on port 4000.'));
