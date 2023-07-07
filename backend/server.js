f196c016c6153f6aa56e2de52d59946204029ee8
Andy Humes <andrew.humes@du.edu>
2023-07-06 22:00:28 -0600

    addroutesforprescriptions

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,17 @@
 const express = require("express");
-const app = express();
 
+const prescriptionsRoutes = require("./routes/prescriptions")
 require('dotenv').config();
 
+const app = express();
 
 // establish middle ware to monitor requests to server and response//
 app.use((req, res, next) => {
-  console.log("Path is " + req.path + " and method is " + req.method)
+  console.log("Path is " + req.path + "and method is " + req.method);
+  next();
 })
 
 //routes /////////////////////////////
-app.get('/', (req, res) => {
-  res.json({mssg:'Route is / and method is ' + req.method})
-}
-);
+app.use('/api/prescriptions/', prescriptionsRoutes)
 
 app.listen(process.env.PORT, () => console.log('listening on port 4000.'));
