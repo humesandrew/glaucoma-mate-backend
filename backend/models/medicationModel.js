e1058a4d13979742587f12ed13f299ed4ecf2f1b
Andy Humes <andrew.humes@du.edu>
2023-09-27 20:13:44 -0600

    addmedmodel

---
+++ backend/models/medicationModel.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const medicationSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    dosage: {
+      type: Number,
+      required: true,
+    },
+    sig: {
+        type: String,
+        required: true,
+    },
+    capColor: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model('Medications', medicationSchema);
