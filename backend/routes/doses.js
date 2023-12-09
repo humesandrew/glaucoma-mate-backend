33a01975c49428ab47a9dedddf400c87585a1e83
Andy Humes <andrew.humes@du.edu>
2023-12-09 15:47:41 -0700

    adddeletedoseroute

--- backend/routes/doses.js
+++ backend/routes/doses.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const mongoose = require('mongoose');
 const Dose = require("../models/doseModel");
 const { 
   createDose,
@@ -18,11 +19,31 @@ router.get("/:id", getDose);
 // Post a dose
 router.post("/", createDose)
 
-// Delete a dose
-router.delete("/:id", (req, res) => {
-  res.json({ mssg: "DELETE a single dose." });
-});
 
+// Delete a dose
+router.delete("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    // Check if the provided ID is valid
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      return res.status(400).json({ error: "Invalid Dose ID" });
+    }
+
+    // Find and delete the dose
+    const deletedDose = await Dose.findByIdAndDelete(id);
+
+    // If the dose was not found
+    if (!deletedDose) {
+      return res.status(404).json({ error: "Dose not found" });
+    }
+
+    res.status(200).json({ message: "Dose deleted successfully", deletedDose });
+  } catch (error) {
+    console.error(error);  // Log the error for debugging purposes
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
 // Update a dose
 router.patch("/:id", (req, res) => {
   res.json({ mssg: "PATCH a single dose." });
